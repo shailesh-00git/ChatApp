17141b6c108dd18d79002187e651043e59744e29
shailesh-00git <sy468698@gmail.com>
2026-04-24 22:26:31 +0545

    inital project setup and home page added

--- README.md
+++ README.md
@@ -42,3 +42,4 @@ Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/bui
    npx create-next-app@latest
    ```
 2.
+
--- src/app/page.js
+++ src/app/page.js
@@ -1,65 +1,51 @@
-import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    <main className="h-screen w-7xl mx-auto p-2">
+      {/* header */}
+      <header className="border flex justify-between items-center py-3 px-8">
+        <h1 className="text-3xl text-cyan-800">ChatOff</h1>
+        <nav className="flex justify-center items-center gap-3">
+          <button className="bg-cyan-800 text-white px-3 rounded-lg py-1">
+            Sign up
+          </button>
+          <button className="bg-cyan-800 text-white px-3 rounded-lg py-1">
+            Login
+          </button>
+        </nav>
+      </header>
+
+      {/* main */}
+      <div className="flex flex-row h-140">
+        <aside className="flex-1 border p-2 flex flex-col gap-3">
+          <div className="bg-cyan-700 text-white rounded p-3">Home</div>
+          <div className="bg-cyan-700 text-white rounded p-3">Home</div>{" "}
+          <div className="bg-cyan-700 text-white rounded p-3">Home</div>{" "}
+          <div className="bg-cyan-700 text-white rounded p-3">Home</div>{" "}
+        </aside>
+        {/* main */}
+        <main className="flex-5 border">
+          <div className="flex flex-col h-full">
+            <div className="flex-1 ">message area</div>
+            <div className="border p-3 flex justify-end items-center gap-2">
+              <span className="bg-cyan-800 text-white px-3 rounded-2xl py-1">
+                +
+              </span>
+              <input
+                type="text"
+                className="px-2 py-1 border  rounded-2xl"
+                placeholder="message"
+              />
+              <button className="bg-cyan-800 text-white px-3 rounded-2xl py-1">
+                send
+              </button>
+            </div>
+          </div>
+        </main>
+      </div>
+    </main>
   );
 }
+
+export default Home;
