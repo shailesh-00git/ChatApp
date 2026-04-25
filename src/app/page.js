230516f6c8db96096f6a381016f2ed9077fdd15c
shailesh-00git <sy468698@gmail.com>
2026-04-25 16:46:49 +0545

    removed the  extra and unwanted code and files

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
 "use client";
-
 import { useState, useEffect, useRef } from "react";
-import { supabase } from "@/lib/supabase";
+import { supabase } from "../lib/supabase";
 
 export default function Home() {
   const [rooms, setRooms] = useState([]);
